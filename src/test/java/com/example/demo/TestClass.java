64d66fa4d75f2d3f3ce916aa9a88903d7fbac65e
akashgup <akash.gupta2@dhl.com>
2022-02-22 19:03:35 +0530

    Update TestClass.java

--- src/test/java/com/example/demo/TestClass.java
+++ src/test/java/com/example/demo/TestClass.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotSame;
 
 import javax.annotation.Priority;
 
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -20,54 +21,22 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@RunWith(SpringJUnit4ClassRunner.class)
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class TestClass {
 	
 	@LocalServerPort
     private int port;
 	
-//	public int add(int a, int b) {
-//		return a + b;
-//	}
-//
-//	public int sub(int a, int b) {
-//		return a - b;
-//	}
-//
-//	@Test
-//	public void testAdd() {
-//		int total = 8;
-//		int sum = add(4, 4);
-//		assertEquals(sum, total);
-//	}
-//
-//	@Test
-//	public void testFailedAdd() {
-//		int total = 9;
-//		int sum = add(10, 5);
-//		assertNotSame(sum, total);
-//	}
-//
-//	@Test
-//	public void testSub() {
-//		int total = 0;
-//		int sub = sub(4, 4);
-//		assertEquals(sub, total);
-//	}
-	
-//	@BeforeEach
-//	void setUp(Method methodname) {
-//		System.out.println("Before method : " + methodname);
-//	}
-//
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
 	
 	@Test
-	void Test1() throws InterruptedException {
+	void Test_Get_1() throws InterruptedException {
 	
 	System.out.println("Function Test1");
-	RestAssured.baseURI = "http://localhost";
-	RestAssured.port = port;
 	RequestSpecification httpRequest = RestAssured.given();
 	Response response = httpRequest.request(Method.GET , "/users?page=1&limit=50");
 	assertEquals(response.statusCode(), 200);
@@ -76,23 +45,14 @@ public class TestClass {
 	}
 	
 	@Test
-	void Test2() throws InterruptedException {
+	void Test_Post_2() throws InterruptedException {
 	
 	System.out.println("Function Test2");
-	RestAssured.baseURI = "http://localhost";
-	RestAssured.port = port;
 	RequestSpecification httpRequest = RestAssured.given();
 	Response response = httpRequest.request(Method.POST, "/users");
 	assertEquals(response.statusCode(), 200);
 	System.out.println(response.asPrettyString());
 		
 	}
-//	
-//	@Test
-//	void Test2() throws InterruptedException {
-//	
-//	System.out.println("Function Test2");
-//		
-//	}
 
 }
